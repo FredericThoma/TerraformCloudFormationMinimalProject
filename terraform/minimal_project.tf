839e3efe347af3153900f0f7538d8f252246ef1f
Frederic Thoma <frederic.thoma0599@gmail.com>
2025-12-05 18:41:11 +0100

    add: subnet, rta

--- terraform/minimal_project.tf
+++ terraform/minimal_project.tf
@@ -33,3 +33,20 @@ resource "aws_route_table" "dev_route_table" {
     Project = "project1"
   }
 }
+
+resource "aws_subnet" "dev_subnet" {
+  vpc_id     = aws_vpc.dev_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "project1_dev_subnet"
+    Environment = "dev"
+    Project = "project1"
+  }
+}
+
+resource "aws_route_table_association" "dev_rta" {
+  subnet_id      = aws_subnet.dev_subnet.id
+  route_table_id = aws_route_table.dev_route_table.id
+}
+
